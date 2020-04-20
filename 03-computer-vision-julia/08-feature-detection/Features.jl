6e59dd2c07452a76aa16240dcfea3fb12de73824
asamanta1920 <asamanta1920@gmail.com>
2020-04-19 22:43:59 -0400

    Changed Feature Detection

--- 03-computer-vision-julia/08-feature-detection/Features.jl
+++ 03-computer-vision-julia/08-feature-detection/Features.jl
@@ -1,13 +1,30 @@
-using ImageFeatures, TestImages, Images, ImageDraw, CoordinateTransformations
+# using ImageFeatures, TestImages, Images, ImageDraw, CoordinateTransformations
+#
+# building = load("building.jpg")
+
+# features_1 = imcorner(building, method=harris);
+#
+# function draw_points(image::AbstractArray, mask::Keypoints; c::Colorant=colorant"yellow")
+#     new_image = copy(image)
+#     new_image[mask] = c
+#     return new_image
+# end
+#
+# draw_points(building, features_1)
+
+using ImageFeatures, TestImages, Images, ImageDraw, CoordinateTransformations, Interact, QuartzImageIO
 
 building = load("building.jpg")
+building_gray = Gray.(building)
 
 features_1 = imcorner(building, method=harris);
+features_1 = .!.!features_1
 
-function draw_points(image::AbstractArray, mask::Keypoints; c::Colorant=colorant"yellow")
-    new_image = copy(image)
-    new_image[mask] = c
-    return new_image
+function draw_points(image::AbstractArray, mask; c::Colorant=colorant"red")
+    new_image = copy(image);
+    new_image[mask] .= c;
+    return new_image;
 end
 
-draw_points(building, features_1)
+features4 = imcorner(building, 0.90, true, method=harris)
+draw_points(building, features4);
